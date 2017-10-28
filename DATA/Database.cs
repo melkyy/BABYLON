1476aada960c871c3cb75da0357ef486fa20281b
505darksoft <eduardo.hley@gmail.com>
2017-10-27 19:56:46 -0500

    clase Database agregada

---
+++ DATA/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace DATA
+{
+    public class Database
+    {
+        private string ConnectionString = string.Empty;
+        public SqlConnection Connection { get; private set; }
+        public Database()
+        {
+
+        }
+        public Database();
+        public SqlConnection getConnection()
+        {
+            if (Connection == null)
+            {
+                Connection = new SqlConnection(ConnectionString);
+            }
+            return Connection;
+        }
+        private void readConfig()
+        {
+            //aquí leer archivo con la configuracion
+        }
+    }
+}
