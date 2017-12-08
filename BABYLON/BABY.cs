8a9ec0584c35feafef4f80dd49f6247009210e6d
505darksoft <eduardo.hley@gmail.com>
2017-12-07 23:20:21 -0600

    enum Agregados en BABYLON >> BABY

---
+++ BABYLON/BABY.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BABYLON
+{
+    public class BABY
+    {
+        public enum enmResultados
+        {
+            OperacionCorrecta,
+            FaltanDatos,
+            ErrorDeConexion,
+            CamposVacios,
+            MetodoNoImplementado,
+            IdYaExistente,
+            ErrorDeSentencia,
+            IdNoExiste,
+            IdPadreNoExiste,
+            RegistrodeSistema,
+            HijosActivos,
+            ErrorDeBaseDeDatos,
+            ArchivoNoExistente,
+            ImagenNoExiste,
+            DatosDuplicados
+        }
+    }
+}
